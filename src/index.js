802fad6ae7a9384ff827b59e5ce9d2cfe4fb5472
Vinicius Berto <vinio2507@gmail.com>
2019-01-16 23:12:28 -0300

    Gerenciamento de saída

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import lodash from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import 
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-   // Add the image to our existing div.
-   var myIcon = new Image();
-   myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-   element.appendChild(myIcon);
-  
+    element.appendChild(btn);
+
     return element;
   }
   
