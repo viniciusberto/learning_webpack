95d9d792dd73e4cace925ed55a42570e93de1e6c
Vinicius Berto <vinio2507@gmail.com>
2019-01-16 23:24:01 -0300

    Limpar a pasta dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,9 +9,10 @@ module.exports = {
   },
   mode: "development",
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      
+
     })
   ],
   output: {
