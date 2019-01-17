69f7597c2c117dee65792ebb2dccda1f07badb9a
Vinicius Berto <vinio2507@gmail.com>
2019-01-16 23:03:20 -0300

    Gestão de Ativos

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import lodash from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import 
+
+function component() {
+    let element = document.createElement('div');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+   // Add the image to our existing div.
+   var myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
